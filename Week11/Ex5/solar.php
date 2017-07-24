baab968011d8d3fe11b208dbae66c3c0830d2019
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2017-07-24 18:35:32 -0400

    Example 6

--- Week11/Ex5/solar.php
+++ Week11/Ex5/solar.php
@@ -1,5 +1,5 @@
 <?php
-$WeatherSource = "https://api.forecast.io/forecast/a8b6abe791df51b4f3db619d706fb564/" . $_GET["lat"] . "," . $_GET["lng"];
+$WeatherSource = "https://api.darksky.net/forecast/fb13eac1f7b996df08bad0f0f154bab4/" . $_GET["lat"] . "," . $_GET["lng"];
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 readfile($WeatherSource);
