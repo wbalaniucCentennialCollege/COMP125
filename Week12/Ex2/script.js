f03e9c0acc7032513d158136a5d4fdf3c972a4ed
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2017-07-25 20:53:28 -0400

    Added Week 12 Examples

---
+++ Week12/Ex2/script.js
@@ -0,0 +1,11 @@
+﻿/*
+    JavaScript 6th Edition
+    Chapter 12
+    Chapter case
+    Life on Rocks Wildlife Cruises
+    Author:
+    Date:
+    Filename: script.js
+*/
+
+$("ul.mainmenu li").children("ul").addClass("show");
---
+++ Week12/Ex3/script.js
@@ -0,0 +1,21 @@
+﻿/*
+    JavaScript 6th Edition
+    Chapter 12
+    Chapter case
+    Life on Rocks Wildlife Cruises
+    Author:
+    Date:
+    Filename: script.js
+*/
+
+//$("ul.mainmenu li").children("ul").addClass("show");
+
+function display(event) {
+    $(event.currentTarget).children("ul").addClass("show");
+}
+
+function hide(event) {
+    $(event.currentTarget).children("ul").removeClass("show");
+}
+
+$("ul.mainmenu li").hover(display, hide);
